a8739ccbf20ac6052ab8defdccdb0a243543e628
Rena <renamarkiewitz@gmail.com>
2020-06-09 21:47:20 +0300

    second commit

---
+++ Comon/Pair.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Comon
+{
+    public class Pair
+    {
+        public int shape { get; set; }
+        public int Orbit { get; set; }
+        public PointClass PointOnA { get; set; }
+        public Pair()
+        {
+
+        }
+        public Pair(int a, int b)
+        {
+            shape = a;
+            Orbit = b;
+        }
+    }
+}
